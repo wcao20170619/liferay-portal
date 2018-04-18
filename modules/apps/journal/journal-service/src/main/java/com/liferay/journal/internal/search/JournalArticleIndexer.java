cef91ec3f7f20511cdb408844eadd00f3fed5828
Istvan Sajtos <istvan.sajtos@liferay.com>
2018-04-18 13:06:33 +0200

    LPS-78119 journal-service: Do not index localized fields without using proper language analyzers
    Also Search in all available locales for JournalArticles
    
    https://issues.liferay.com/browse/LPS-78119

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/search/JournalArticleIndexer.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/search/JournalArticleIndexer.java
@@ -40,6 +40,7 @@ import com.liferay.portal.kernel.dao.orm.Property;
 import com.liferay.portal.kernel.dao.orm.PropertyFactoryUtil;
 import com.liferay.portal.kernel.dao.orm.RestrictionsFactoryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.module.configuration.ConfigurationProvider;
@@ -449,22 +450,33 @@ public class JournalArticleIndexer
 			return Collections.emptyMap();
 		}
 
-		String localizedField = Field.getLocalizedName(
-			searchContext.getLocale(), field);
+		Set<Locale> availableLocales = new HashSet<>();
+
+		long[] groupIds = searchContext.getGroupIds();
+
+		if (groupIds == null) {
+			availableLocales = LanguageUtil.getAvailableLocales();
+		}
+		else {
+			for (long groupId : groupIds) {
+				availableLocales.addAll(
+					LanguageUtil.getAvailableLocales(groupId));
+			}
+		}
 
 		Map<String, Query> queries = new HashMap<>();
 
 		if (Validator.isNull(searchContext.getKeywords())) {
 			BooleanQuery localizedQuery = new BooleanQueryImpl();
 
-			Query query = localizedQuery.addTerm(field, value, like);
+			for (Locale locale : availableLocales) {
+				String localizedField = Field.getLocalizedName(locale, field);
 
-			queries.put(field, query);
+				Query localizedFieldQuery = localizedQuery.addTerm(
+					localizedField, value, like);
 
-			Query localizedFieldQuery = localizedQuery.addTerm(
-				localizedField, value, like);
-
-			queries.put(field, localizedFieldQuery);
+				queries.put(localizedField, localizedFieldQuery);
+			}
 
 			BooleanClauseOccur booleanClauseOccur = BooleanClauseOccur.SHOULD;
 
@@ -475,9 +487,13 @@ public class JournalArticleIndexer
 			searchQuery.add(localizedQuery, booleanClauseOccur);
 		}
 		else {
-			Query query = searchQuery.addTerm(localizedField, value, like);
+			for (Locale locale : availableLocales) {
+				String localizedField = Field.getLocalizedName(locale, field);
 
-			queries.put(field, query);
+				Query query = searchQuery.addTerm(localizedField, value, like);
+
+				queries.put(field, query);
+			}
 		}
 
 		return queries;
@@ -537,9 +553,6 @@ public class JournalArticleIndexer
 
 		document.addUID(CLASS_NAME, classPK);
 
-		String articleDefaultLanguageId = LocalizationUtil.getDefaultLanguageId(
-			journalArticle.getDocument());
-
 		String[] languageIds = LocalizationUtil.getAvailableLanguageIds(
 			journalArticle.getDocument());
 
@@ -549,12 +562,6 @@ public class JournalArticleIndexer
 			String description = journalArticle.getDescription(languageId);
 
 			String title = journalArticle.getTitle(languageId);
-
-			if (languageId.equals(articleDefaultLanguageId)) {
-				document.addText(Field.CONTENT, content);
-				document.addText(Field.DESCRIPTION, description);
-				document.addText("defaultLanguageId", languageId);
-			}
 
 			document.addText(
 				LocalizationUtil.getLocalizedName(Field.CONTENT, languageId),
@@ -588,6 +595,12 @@ public class JournalArticleIndexer
 			"ddmStructureKey", journalArticle.getDDMStructureKey());
 		document.addKeyword(
 			"ddmTemplateKey", journalArticle.getDDMTemplateKey());
+
+		String articleDefaultLanguageId = LocalizationUtil.getDefaultLanguageId(
+			journalArticle.getDocument());
+
+		document.addText("defaultLanguageId", articleDefaultLanguageId);
+
 		document.addDate("displayDate", journalArticle.getDisplayDate());
 		document.addKeyword("head", JournalUtil.isHead(journalArticle));
 
