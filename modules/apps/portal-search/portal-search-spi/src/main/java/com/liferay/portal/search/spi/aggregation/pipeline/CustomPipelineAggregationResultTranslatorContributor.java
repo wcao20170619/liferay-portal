2fe6cf4fb01bd2a06022b7fb20a4ddfcbc8e9af2
Michael Han <michael.han@liferay.com>
2019-02-08 14:10:17 -0600

    LPS-89501 CUSTOM AGGS portal-search-spi: add extension points for adding custom queries and aggregations

---
+++ modules/apps/portal-search/portal-search-spi/src/main/java/com/liferay/portal/search/spi/aggregation/pipeline/CustomPipelineAggregationResultTranslatorContributor.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.spi.aggregation.pipeline;
+
+import com.liferay.portal.search.aggregation.AggregationResult;
+import com.liferay.portal.search.aggregation.pipeline.PipelineAggregation;
+import com.liferay.portal.search.aggregation.pipeline.PipelineAggregationResultTranslator;
+
+/**
+ * @author Michael C. Han
+ */
+public interface CustomPipelineAggregationResultTranslatorContributor
+	<S extends AggregationResult, T> {
+
+	public String getAggregationClassName();
+
+	public AggregationResult translate(
+		PipelineAggregation customPipelineAggregation, T aggregation,
+		PipelineAggregationResultTranslator
+			pipelineAggregationResultTranslator);
+
+}
