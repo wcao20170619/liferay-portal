5af15a4ffe0b8a4e357991bee71a111f35f7d64a
weicao <wade.cao@liferay.com>
2019-03-05 10:52:51 -0800

    LPS-91264 portal-search:add implementation of classes for Score Function subsystem
    https://issues.liferay.com/browse/LPS-91264

---
+++ modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/query/function/score/GuassianDecayScoreFunctionImpl.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.internal.query.function.score;
+
+import com.liferay.portal.search.query.function.score.GuassianDecayScoreFunction;
+import com.liferay.portal.search.query.function.score.ScoreFunctionTranslator;
+
+/**
+ * @author Michael C. Han
+ */
+public class GuassianDecayScoreFunctionImpl
+	extends DecayScoreFunctionImpl implements GuassianDecayScoreFunction {
+
+	public GuassianDecayScoreFunctionImpl(
+		String field, Object origin, Object scale, Object offset) {
+
+		super(field, origin, scale, offset);
+	}
+
+	public GuassianDecayScoreFunctionImpl(
+		String field, Object origin, Object scale, Object offset,
+		Double decay) {
+
+		super(field, origin, scale, offset, decay);
+	}
+
+	@Override
+	public <T> T accept(ScoreFunctionTranslator<T> scoreFunctionTranslator) {
+		return scoreFunctionTranslator.translate(this);
+	}
+
+}
