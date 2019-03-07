5b3fe7082f451019e00116d518fc1b04623e022e
weicao <wade.cao@liferay.com>
2019-03-07 15:00:48 -0800

    LPS-91263 portal-search:add implementation of classes for Sort subsystem
    https://issues.liferay.com/browse/LPS-91263

---
+++ modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/sort/SortImpl.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.internal.sort;
+
+import com.liferay.portal.search.sort.Sort;
+import com.liferay.portal.search.sort.SortOrder;
+import com.liferay.portal.search.sort.SortVisitor;
+
+/**
+ * @author Michael C. Han
+ */
+public abstract class SortImpl implements Sort {
+
+	public abstract <T> T accept(SortVisitor<T> sortVisitor);
+
+	public SortOrder getSortOrder() {
+		return _sortOrder;
+	}
+
+	public void setSortOrder(SortOrder sortOrder) {
+		_sortOrder = sortOrder;
+	}
+
+	private SortOrder _sortOrder = SortOrder.ASC;
+
+}
