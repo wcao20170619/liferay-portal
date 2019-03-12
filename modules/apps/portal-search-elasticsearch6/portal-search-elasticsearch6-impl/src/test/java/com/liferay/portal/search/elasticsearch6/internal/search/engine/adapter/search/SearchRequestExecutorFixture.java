f42cb4bc9128ad074db6ac05f10ead219c26940e
weicao <wade.cao@liferay.com>
2019-03-12 13:50:50 -0700

    LPS-91259 portal-search-elasticsearch6-impl:modify test fixture to add new components
    https://issues.liferay.com/browse/LPS-91259

--- modules/apps/portal-search-elasticsearch6/portal-search-elasticsearch6-impl/src/test/java/com/liferay/portal/search/elasticsearch6/internal/search/engine/adapter/search/SearchRequestExecutorFixture.java
+++ modules/apps/portal-search-elasticsearch6/portal-search-elasticsearch6-impl/src/test/java/com/liferay/portal/search/elasticsearch6/internal/search/engine/adapter/search/SearchRequestExecutorFixture.java
@@ -35,6 +35,7 @@ import com.liferay.portal.search.elasticsearch6.internal.stats.DefaultStatsTrans
 import com.liferay.portal.search.elasticsearch6.internal.stats.StatsTranslator;
 import com.liferay.portal.search.engine.adapter.search.SearchRequestExecutor;
 import com.liferay.portal.search.internal.aggregation.AggregationResultsImpl;
+import com.liferay.portal.search.internal.highlight.HighlightsImpl;
 import com.liferay.portal.search.internal.hits.SearchHitBuilderFactoryImpl;
 import com.liferay.portal.search.internal.hits.SearchHitsBuilderFactoryImpl;
 import com.liferay.portal.search.internal.legacy.stats.StatsRequestBuilderFactoryImpl;
@@ -292,6 +293,7 @@ public class SearchRequestExecutorFixture {
 							setStatsTranslator(statsTranslator);
 						}
 					});
+				setHighlights(new HighlightsImpl());
 				setSearchHitBuilderFactory(new SearchHitBuilderFactoryImpl());
 				setSearchHitsBuilderFactory(new SearchHitsBuilderFactoryImpl());
 				setSearchResponseTranslator(
