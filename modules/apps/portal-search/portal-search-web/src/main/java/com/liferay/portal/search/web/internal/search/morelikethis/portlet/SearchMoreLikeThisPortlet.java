7705cc1b4953c3624c0cd9be1b2b6d02f9a0b330
weicao <wade.cao@liferay.com>
2019-06-26 07:35:02 -0700

    tmp commit

---
+++ modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/search/morelikethis/portlet/SearchMoreLikeThisPortlet.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.web.internal.search.morelikethis.portlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.portal.search.web.internal.search.morelikethis.constants.SearchMoreLikeThisPortletKeys;
+
+/**
+ * @author Wade Cao
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.add-default-resource=true",
+	    "com.liferay.portlet.css-class-wrapper=portlet-search-more-like-this",
+		"com.liferay.portlet.display-category=category.search",
+		"com.liferay.portlet.icon=/icons/search.png",
+		"com.liferay.portlet.instanceable=true",
+		"com.liferay.portlet.layout-cacheable=true",
+		"com.liferay.portlet.preferences-owned-by-group=true",
+		"com.liferay.portlet.private-request-attributes=false",
+		"com.liferay.portlet.private-session-attributes=false",
+		"com.liferay.portlet.restore-current-view=false",
+		"com.liferay.portlet.use-default-template=true",
+		"javax.portlet.display-name=Search More Like This",
+		"javax.portlet.expiration-cache=0",
+		"javax.portlet.init-param.template-path=/META-INF/resources/",
+		"javax.portlet.init-param.view-template=/search/morelikethis/view.jsp",
+		"javax.portlet.name=" + SearchMoreLikeThisPortletKeys.MORE_LIKE_THIS_PORTLET,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=guest,power-user,user",
+		"javax.portlet.supports.mime-type=text/html"
+	},
+	service = Portlet.class
+)
+
+public class SearchMoreLikeThisPortlet extends MVCPortlet {
+	
+}
