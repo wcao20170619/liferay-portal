33552022c2fb899d645a9dde338dbeec765e5421
Kevin Tan <kevin.tan@liferay.com>
2019-08-05 17:35:43 -0700

    LPS-98543 portal-search-related-results-web: Initial setup

---
+++ modules/apps/portal-search/portal-search-related-results-web/src/main/java/com/liferay/portal/search/related/results/web/portlet/SearchRelatedResultsPortlet.java
@@ -0,0 +1,29 @@
+package com.liferay.portal.search.related.results.web.portlet;
+
+import com.liferay.portal.search.related.results.web.constants.SearchRelatedResultsPortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Kevin Tan
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.search",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + SearchRelatedResultsPortletKeys.SEARCH_RELATED_RESULTS,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class SearchRelatedResultsPortlet extends MVCPortlet {
+}
