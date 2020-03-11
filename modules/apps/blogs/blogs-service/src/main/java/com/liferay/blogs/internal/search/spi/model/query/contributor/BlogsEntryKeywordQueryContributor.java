ed6ef2dd992da1681e93453e6bca42f6e709abea
Joshua Cords <joshua.cords@liferay.com>
2020-03-11 16:55:38 -0700

    LPS-111601 blogs-service: encapsulate query with entryClassName
    https://issues.liferay.com/browse/LPS-111601

--- modules/apps/blogs/blogs-service/src/main/java/com/liferay/blogs/internal/search/spi/model/query/contributor/BlogsEntryKeywordQueryContributor.java
+++ modules/apps/blogs/blogs-service/src/main/java/com/liferay/blogs/internal/search/spi/model/query/contributor/BlogsEntryKeywordQueryContributor.java
@@ -14,9 +14,15 @@
 
 package com.liferay.blogs.internal.search.spi.model.query.contributor;
 
+import com.liferay.blogs.model.BlogsEntry;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.search.BooleanClauseOccur;
 import com.liferay.portal.kernel.search.BooleanQuery;
 import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.ParseException;
 import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.kernel.search.generic.BooleanQueryImpl;
+import com.liferay.portal.kernel.search.generic.TermQueryImpl;
 import com.liferay.portal.search.query.QueryHelper;
 import com.liferay.portal.search.spi.model.query.contributor.KeywordQueryContributor;
 import com.liferay.portal.search.spi.model.query.contributor.helper.KeywordQueryContributorHelper;
@@ -43,12 +49,29 @@ public class BlogsEntryKeywordQueryContributor
 		SearchContext searchContext =
 			keywordQueryContributorHelper.getSearchContext();
 
+		BooleanQuery searchTermQuery = new BooleanQueryImpl();
+
 		_queryHelper.addSearchLocalizedTerm(
-			booleanQuery, searchContext, Field.CONTENT, false);
+			searchTermQuery, searchContext, Field.CONTENT, false);
 		_queryHelper.addSearchTerm(
-			booleanQuery, searchContext, Field.SUBTITLE, false);
+			searchTermQuery, searchContext, Field.SUBTITLE, false);
 		_queryHelper.addSearchLocalizedTerm(
-			booleanQuery, searchContext, Field.TITLE, false);
+			searchTermQuery, searchContext, Field.TITLE, false);
+
+		BooleanQuery entryClassQuery = new BooleanQueryImpl();
+
+		try {
+			entryClassQuery.add(
+				new TermQueryImpl(
+					Field.ENTRY_CLASS_NAME, BlogsEntry.class.getName()),
+				BooleanClauseOccur.MUST);
+			entryClassQuery.add(searchTermQuery, BooleanClauseOccur.MUST);
+
+			booleanQuery.add(entryClassQuery, BooleanClauseOccur.SHOULD);
+		}
+		catch (ParseException parseException) {
+			throw new SystemException(parseException);
+		}
 	}
 
 	@Reference
